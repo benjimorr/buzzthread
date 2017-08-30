646d11b42cd6ee5a172d6ceeca61588835054f95
Ben Morrison <benmorrison0@gmail.com>
2017-08-30 17:30:08 -0400

    Assignment 18 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
       @posts = Post.all
+      @posts.find_each.with_index do |post, index|
+          if index % 5 == 0
+              post.title = "SPAM"
+              post.save
+          end
+      end
   end
 
   def show
